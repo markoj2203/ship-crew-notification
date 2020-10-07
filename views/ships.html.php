852d9010b220870a16f2b7c3fe3d071861f8e4fc
unknown <markoj2203@gmail.com>
2020-10-07 10:53:13 +0200

    some code correction

--- views/ships.html.php
+++ views/ships.html.php
@@ -4,15 +4,9 @@
   <div class="row mx-lg-n5">
     <div class="col py-3 px-lg-5 border bg-light" style="height:340px;overflow-y: auto;">
     <ul class="list-group">
-            <li class="list-group-item active">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
+            <li class="list-group-item active">Ship1</li>
+            <li class="list-group-item">Ship2</li>
+            <li class="list-group-item">Ship3</li>
         </ul>
     </div>
     <div class="col py-3 px-lg-5 border bg-light">
